b835c009b0796653d657ba160914fec7892bae03
Nguyen Van Hung <nv-hung@linkstaff.co.jp>
2024-01-10 16:48:10 +0900

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,42 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import liff from "@line/liff";
 import { useNavigate } from "react-router-dom";
 
+const initialProfile = {
+    displayName: '',
+    userId: ''
+}
+
 const Home = () => {
-    // const [profile,setProfile] = useState({});
+    const [profile,setProfile] = useState(initialProfile);
     const navigate = useNavigate();
 
-    const initLine = () => {
-        liff.init({liffId: import.meta.env.VITE_LINE_ID}, async () => {
-            if(!liff.isLoggedIn()) {
-                navigate('/line-login-page')
-            } else {
-                const profile = await liff.getProfile();
-                console.log(profile);
-            }
-        })
+    const logout = () => {
+        liff.logout();
+        navigate('/line-login-page')
     }
+
     useEffect(() => {
+        const initLine = () => {
+            liff.init({liffId: import.meta.env.VITE_LINE_ID}, async () => {
+                if(!liff.isLoggedIn()) {
+                    navigate('/line-login-page')
+                } else {
+                    const profile = await liff.getProfile();
+                    setProfile(profile);
+                }
+            })
+        }
         initLine()
-    }, [])
+    }, [navigate])
     
 
     return (
         <div className="container">
             <div className="box">
-                <span>Name:</span>
-                <span></span>
+                <span>Name: { profile.displayName }</span><br />
+                <span>ID: { profile.userId }</span><br />
+                <button className="" onClick={logout}>Logout</button>
             </div>
         </div>
     )
