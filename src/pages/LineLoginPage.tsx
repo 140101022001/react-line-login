2d4bf9cc6b7a3c199b90c7b36cea3006265b8407
Nguyen Van Hung <nv-hung@linkstaff.co.jp>
2024-01-10 16:52:53 +0900

    first commit

--- src/pages/LineLoginPage.tsx
+++ src/pages/LineLoginPage.tsx
@@ -2,16 +2,17 @@ import liff from "@line/liff";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const LineLoginPage = () => {
     const navigate = useNavigate();
+
+    const lineLogin = () => {
+        liff.login();
+    }
     useEffect(() => {
         const initLine = () => {
             liff.init({liffId: import.meta.env.VITE_LINE_ID}, async () => {
                 if(liff.isLoggedIn()) {
                     navigate('/')
-                } else {
-                    liff.login();
                 }
             })
         }
@@ -19,7 +20,9 @@ const LineLoginPage = () => {
     }, [navigate])
 
     return (
-        <div>LineLoginPage</div>
+        <div>
+            <button onClick={lineLogin}>Line Login</button>
+        </div>
     )
 }
 
